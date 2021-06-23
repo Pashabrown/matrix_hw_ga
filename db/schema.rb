32ab904e61c750a1c0590b1d6e04c89f54d54ce4
Pasha Brown <pashabrown@gmail.com>
2021-06-23 13:25:34 -0700

    Commit 4: Renamed the column

--- db/schema.rb
+++ db/schema.rb
@@ -9,10 +9,18 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-####
-ActiveRecord::Schema.define(version: 0) do
+
+ActiveRecord::Schema.define(version: 2021_06_23_202219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "img_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
